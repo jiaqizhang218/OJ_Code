f7d50b6ee7354ebc1e2083d16e8d2caf01eb52be
zjqaq <33285580+zjqaq@users.noreply.github.com>
2018-05-29 18:27:03 +0800

    Update bzoj 4653.cpp

--- BZOJ/bzoj 4653.cpp
+++ BZOJ/bzoj 4653.cpp
@@ -3,7 +3,6 @@ using namespace std;
 #define debug(...) fprintf(stderr,__VA_ARGS__)
 #define rep(i,a,b) for(register int i=(a),i##_end=(b);i<=i##_end;++i)
 #define per(i,a,b) for(register int i=(a),i##_end=(b);i>=i##_end;--i)
-#define rpg(i,x) for(register int i=fir[x];i;i=e[i].nxt)
 typedef pair<int,int> PII;
 typedef long long LL;
 typedef unsigned long long ull;
@@ -13,65 +12,67 @@ template <typename T> inline void rd(T &x) {
     while(isdigit(CH)) {x=((x+(x<<2))<<1)+(CH-48);CH=getchar();}
     x=(f==1)?x:-x;return;
 }
-template <typename T> inline bool chMax(T &a,T &b) {
-    return a<b?a=b,true:false;
-}
-template <typename T> inline bool chMin(T &a,T &b) {
+template <typename T> inline bool chMin(T &a,T b) {
     return a>b?a=b,true:false;
-} 
-const int MXN = 1e5 + 5;
-int n,fir[MXN],edge_cnt,vis[MXN],pri[MXN],pri_cnt;
-struct edge {
-	int to,nxt,w;
-}e[MXN<<1];
-inline void AE(int u,int v,int w) {
-	e[++edge_cnt].to=v;e[edge_cnt].nxt=fir[u];fir[u]=edge_cnt;e[edge_cnt].w=w;
 }
-ull _rand() {
-	ull ret=0,x=(1<<8)-1;
-	rep(i,1,8) ret=ret<<8|(rand()&x);
-	return ret;
+const int MXN = 2e6 + 5,INF=0x7fffffff;
+struct node {
+	int l,r,id,len;
+}a[MXN];
+#define lson k<<1,l,mid
+#define rson k<<1|1,mid+1,r
+inline bool cmp(const node &a,const node &b) {
+	return a.len<b.len;	
 }
-map<int,ull> id; map<ull,int> f;
-LL ans;
-void dfs(int x,int fa,int val,ull cd) {
-	if(val) {
-		int m = sqrt(val);
-		for(int i=1;pri[i]<=m;++i) {
-			while(val%pri[i]==0) {
-				if(!id.count(pri[i])) id[pri[i]]=_rand();
-				cd ^= id[pri[i]];
-				val /= pri[i];
-			}
-		}
-		if(val > 1) {
-			if(!id.count(val)) id[val]=_rand();
-			cd ^= id[val];
-		}
+int tag[MXN<<2],n,m,mx[MXN<<2],cnt,gery[MXN<<2],ans=INF;
+inline void pushup(int k) {
+	mx[k]=max(mx[k<<1],mx[k<<1|1]);
+}
+inline void pushdown(int k) {
+	if(tag[k]) {
+		tag[k<<1]+=tag[k];tag[k<<1|1]+=tag[k];
+		mx[k<<1]+=tag[k];mx[k<<1|1]+=tag[k];
+		tag[k]=0;
 	}
-	if(!f.count(cd)) f[cd]=1;
-	else ans += f[cd]++;
-	rpg(i,x) if(e[i].to!=fa) dfs(e[i].to,x,e[i].w,cd);
 }
-
+inline void update(int k,int l,int r,int ql,int qr,int val) {
+	if(l>=ql&&r<=qr) {
+		tag[k]+=val,mx[k]+=val;
+		return;
+	}
+	int mid=l+r>>1;
+	pushdown(k);
+	if(qr<=mid) update(lson,ql,qr,val);
+	else if(ql>mid) update(rson,ql,qr,val);
+	else update(lson,ql,mid,val),update(rson,mid+1,qr,val);
+	pushup(k);
+}
 int main() {
 #ifdef iloi
 	freopen("test.in","r",stdin);
 #endif
-	vis[1] = 1;
-	for(int i=2;i<MXN;++i) {
-		if(!vis[i]) {
-			pri[++pri_cnt]=i;
-		}
-		for(int j=1;j<=pri_cnt&&pri[j]*i<MXN;++j) {
-			vis[pri[j]*i]=1;
-			if(i%pri[j]==0) break;
+	rd(n),rd(m);
+	rep(i,1,n) {
+		rd(a[i].l),rd(a[i].r),a[i].len=a[i].r-a[i].l+1;
+		gery[++cnt]=a[i].l,gery[++cnt]=a[i].r;
+	}
+	sort(gery+1,gery+1+cnt); cnt=unique(gery+1,gery+1+cnt)-gery-1;
+//	cerr<<"yes"<<endl;
+	rep(i,1,n) {
+		a[i].l=lower_bound(gery+1,gery+cnt+1,a[i].l)-gery;
+		a[i].r=lower_bound(gery+1,gery+cnt+1,a[i].r)-gery;
+	}
+//	debug("gery[cnt = %d] = %d\n",cnt,gery[cnt]);
+	sort(a+1,a+1+n,cmp);
+	int fc=1;
+	rep(i,1,n) {
+		update(1,1,cnt,a[i].l,a[i].r,1);
+		while(mx[1]>=m) {
+			chMin(ans,a[i].len-a[fc].len);
+			update(1,1,cnt,a[fc].l,a[fc].r,-1);
+			fc++;
 		}
 	}
-	rd(n);	srand(time(0));
-	for(int i=1,x,y,z;i<n;++i) {
-		rd(x),rd(y),rd(z); AE(x,y,z),AE(y,x,z);
-	}
-	dfs(1,0,0,0);
-	printf("%lld\n",ans*2);
+	if(ans==INF) ans=-1;
+	cout<<ans;
 }
